8e20f30c9c96cc8f3a70fb4f02f09c42590adfa2
Stephanie Nemeth <traumverloren@gmail.com>
2016-08-21 16:58:00 +0200

    add more fun to geolize js

--- main.js
+++ main.js
@@ -17,10 +17,13 @@ function swapStyleSheet() {
   if (style === "css/main.css") {
 	 document.getElementById('stylesheet').setAttribute('href', 'css/geolize.css');
    document.getElementById('btn').innerHTML = 'Get me outta here';
+   document.getElementById('job-title').innerHTML = 'InterWebz Developer';
    clearInterval(randomize);
   } else {
   	document.getElementById('stylesheet').setAttribute('href', 'css/main.css');
     document.getElementById('btn').innerHTML = 'CSS like it\'s 1996';
+    document.getElementById('job-title').innerHTML = 'Web Developer';
+
   }
   document.getElementById('btn').blur();
 }
