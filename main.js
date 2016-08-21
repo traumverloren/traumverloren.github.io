43a68e6e3bd906698706ba2ed7f8ad569abfbf99
Stephanie Nemeth <traumverloren@gmail.com>
2016-08-21 11:34:45 +0200

    fix js, add portfolio page, wip

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-
-var colors = ['Aqua', 'Gold', 'Aquamarine', 'Tomato', 'SpringGreen', 'DeepPink', 'DodgerBlue', 'Chartreuse'];
-
-function randomBackgroundColor() {
-  document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-  console.log("OH HAI");
+var randomize = function () {
+  window.setInterval(randomBackgroundColor, 2000);
 }
 
-setInterval(randomBackgroundColor, 2000);
+var colors = ['Aqua', 'Gold', 'Aquamarine', 'Tomato', 'SpringGreen', 'DeepPink', 'DodgerBlue', 'Chartreuse'];
+function randomBackgroundColor() {
+  var style = document.getElementById('stylesheet').getAttribute('href');
+
+  if (style === "css/main.css") {
+    document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    console.log("OH HAI");
+  }
+}
+
+function swapStyleSheet() {
+  var style = document.getElementById('stylesheet').getAttribute('href');
+  if (style === "css/main.css") {
+	 document.getElementById('stylesheet').setAttribute('href', 'css/geolize.css');
+   document.getElementById('btn').innerHTML = 'Get me outta here';
+   clearInterval(randomize);
+  } else {
+  	document.getElementById('stylesheet').setAttribute('href', 'css/main.css');
+    document.getElementById('btn').innerHTML = 'CSS like it\'s 1996';
+  }
+  document.getElementById('btn').blur();
+}
+
+randomize();
