d505154ad3dd1d44bc9c8d29fa0354b91fdb2fa8
Stephanie Nemeth <traumverloren@gmail.com>
2017-07-11 11:14:31 +0200

    add basic service worker installation and cache for offline use

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+// Initiate a Service Worker to cache it!
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+  .then(function(reg) {
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
 var randomize = function () {
   window.setInterval(randomBackgroundColor, 500);
 }
---
+++ sw.js
@@ -0,0 +1,29 @@
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/portfolio.html',
+        '/css/',
+        '/css/geolize.css',
+        '/css/main.css',
+        '/css/portfolio.css',
+        '/main.js',
+        '/img/',
+        '/img/code-background.gif',
+        '/img/gilbert_tessellation.svg.png',
+        '/img/main-background.gif'
+      ]);
+    })
+  );
+});
+
+this.addEventListener('fetch', function(event) {
+  console.log("REQUESTING:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
