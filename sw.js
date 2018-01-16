10a8ca6996bfe6781b708c93129e430e2733e29f
Stephanie Nemeth <traumverloren@gmail.com>
2018-01-16 17:41:02 +0600

    simple but with some ideas commented out

--- sw.js
+++ sw.js
@@ -1,23 +1,23 @@
-this.addEventListener('install', function(event) {
+this.addEventListener("install", function(event) {
   event.waitUntil(
-    caches.open('v1').then(function(cache) {
+    caches.open("v2").then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/portfolio.html',
-        '/css/geolize.css',
-        '/css/main.css',
-        '/css/portfolio.css',
-        '/main.js',
-        '/img/code-background.gif',
-        '/img/gilbert_tessellation.svg',
-        '/img/main-background.gif'
+        "/",
+        "/index.html",
+        "/portfolio.html",
+        "/css/geolize.css",
+        "/css/main.css",
+        "/css/portfolio.css",
+        "/main.js",
+        "/img/code-background.gif",
+        "/img/gilbert_tessellation.svg",
+        "/img/main-background.gif"
       ]);
     })
   );
 });
 
-this.addEventListener('fetch', function(event) {
+this.addEventListener("fetch", function(event) {
   console.log("REQUESTING:", event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
